e12f07838109a7962b35337938faf614f19e4e34
seanraguilar <seanraguilar1999@gmail.com>
2020-04-15 14:28:26 -0600

    Updated stateMachine.h

--- project/stateMachine.h
+++ project/stateMachine.h
@@ -2,5 +2,12 @@
 #define stateMachine_included
 
 void state_advance();
+//void toggle();
+//void toggle_green();
+//void toggle_red();
+void reset_state();
+void led_state(unsigned char g, unsigned char r);
+void delay(unsigned int t);
+void dim();
 
 #endif // included
